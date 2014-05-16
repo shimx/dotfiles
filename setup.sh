4b5c81a88137a40ec550ccbe3c916b73de9da3ce
shimx <shimura.32@gmail.com>
2014-05-16 21:54:01 +0900

    add vim, zsh setup. add make symlink in loop

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
-ln -s ~/dotfiles/.zshenv ~/.zshenv
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.zsh ~/.zsh
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# zsh
+touch $HOME/dotfiles/.zsh/zshenv.local
+touch $HOME/dotfiles/.zsh/zshrc.local
+
+# vim
+mkdir $HOME/dotfiles/.vim
+git submodule add git://github.com/Shougo/neobundle.vim $HOME/dotfiles/.vim/bundle/neobundle.vim
+git submodule add git://github.com/honza/vim-snippets.git $HOME/dotfiles/.vim/bundle/vim-snippets
+
+# make symlink
+for file in `find $HOME/dotfiles -maxdepth 1 -name '.*' | grep -v 'dotfiles/.git' | perl -nle 'm!dotfiles/(.+)$! and print $1'`; do
+    ln -s $HOME/dotfiles/$file $HOME/$file
+done
 
