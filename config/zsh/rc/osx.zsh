67daa4d536b71a303d1c934bf3f43d60a43a97f7
shimx <shimura.32@gmail.com>
2016-07-18 23:34:24 +0900

    add pyenv rbenv

--- config/zsh/rc/osx.zsh
+++ config/zsh/rc/osx.zsh
@@ -1,6 +1,19 @@
+# pyenv
+# you need to install pyenv by homebrew
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# rbenv
+# you need to install rbenv by homebrew
+export RBENV_ROOT="$HOME/.rbenv"
+export PATH="$RBENV_ROOT/bin:$PATH"
+eval "$(rbenv init -)"
+
 # plenv
 # you need to install plenv by homebrew
 #   and cpanm install by homebrew too
 # http://tweeeety.hateblo.jp/entry/2015/05/06/022937
-export PATH="$HOME/.plenv/bin:$PATH"
+export PLENV_ROOT="$HOME/.plenv"
+export PATH="$PLENV_ROOT/bin:$PATH"
 eval "$(plenv init -)"
